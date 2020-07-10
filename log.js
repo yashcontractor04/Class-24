a1666510abf45b1cd6e0f229d32cb700c5cf024f
yashcontractor04 <67055888+yashcontractor04@users.noreply.github.com>
2020-07-10 17:53:49 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,28 @@
+class Log{
+    constructor(x,y,height,angle){
+        var options={
+            "restitution": 0.6,
+            "friction" : 1.4,
+            "density": 1.0
+        }
+        this.body = Bodies.rectangle(x,y,20,height,options);
+        this.width = 20;
+        this.height = height;
+        Matter.Body.setAngle(this.body, angle);
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        rotate(angle);
+        fill("blue");
+        strokeWeight(2);
+        stroke("red");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+};
